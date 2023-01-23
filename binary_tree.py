96c89c57b62b0520b14116156ce630ef475cbd43
cmartinezal <cmartinez@teleworx.com>
2023-01-23 10:43:33 +0800

    Update python example

--- binary_tree.py
+++ binary_tree.py
@@ -52,13 +52,23 @@ class TreeNode:
         print(self.value, end=' ')
 
 #Test case
+''' Construct the following tree (same as in png image Binary-Tree.png)
+              1
+            /   \
+           /     \
+          2       3
+         / \     / \
+        /   \   /   \
+       4     5 6     7
+'''
+
 root = TreeNode(1)
 root.left = TreeNode(2)
 root.right = TreeNode(3)
 root.left.left = TreeNode(4)
 root.left.right = TreeNode(5)
-root.left.right.right = TreeNode(6)
-root.right.left = TreeNode(7)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
 
 print("Pre order Traversal: ", end="")
 root.traverse_pre_order()
